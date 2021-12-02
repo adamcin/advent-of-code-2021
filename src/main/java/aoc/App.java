f35e4fcc417a330c46ff1d011db1794469edaac1
Mark Adamcin <adamcin@users.noreply.github.com>
2021-12-02 15:45:25 +0000

    removed java stuff

--- src/main/java/aoc/App.java
+++
@@ -1,7 +0,0 @@
-package aoc;
-
-public class App {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/main/java/aoc/DayOne.java
+++
@@ -1,9 +0,0 @@
-package aoc;
-
-import java.nio.file.Path;
-
-public class DayOne {
-    public int countIncreases(final Path input) {
-        return 1;
-    }
-}
