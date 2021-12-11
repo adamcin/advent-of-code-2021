5a3835be72e141b91ad63046f87eb5fa2f5a4049
Mark Adamcin <adamcin@users.noreply.github.com>
2021-12-11 05:00:53 +0000

    fixed ts test

--- ts-src/day01.ts
+++ ts-src/day01.ts
@@ -2,4 +2,5 @@ import { Observable } from "rxjs";
 import { map, filter } from 'rxjs/operators';
 import { readTestInputLines } from "./util";
 
-export const readInput = (): Observable<number> => readTestInputLines('day-01').pipe(map(parseInt));
+export const readInput = (): Observable<number> => readTestInputLines('day-01').pipe(
+    map(value => parseInt(value)));
