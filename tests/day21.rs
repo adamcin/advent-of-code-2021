6e502af0e9a357e88f7d337279cdb1f09e9c0e5a
Mark Adamcin <adamcin@users.noreply.github.com>
2022-01-03 02:52:02 +0000

    completed day 21

--- tests/day21.rs
+++ tests/day21.rs
@@ -2,15 +2,17 @@ mod common;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Player {
+    id: usize,
     turns: usize, // n of turns completed before this turn
     index: usize,
     score: usize,
 }
 
 impl Player {
-    fn new(pos: usize, turns: usize) -> Self {
+    fn new(id: usize, pos: usize, turns: usize) -> Self {
         assert!(pos >= 1 && pos <= 10);
         Self {
+            id: id,
             turns: turns,
             index: pos - 1,
             score: 0,
@@ -20,10 +22,15 @@ impl Player {
     fn play(&self, spaces: usize) -> Self {
         let new_index = (self.index + spaces) % 10;
         Self {
+            id: self.id,
             turns: self.turns + 1,
             index: new_index,
             score: self.score + new_index + 1,
         }
+    }
+
+    fn get_score(&self) -> usize {
+        self.score
     }
 
     fn num_rolls(&self) -> usize {
@@ -34,13 +41,12 @@ impl Player {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Die {
     Determ { seed: usize, side: usize },
-    Hyper,
+    Hyper { comb: u128 },
 }
 
 use Die::*;
 
 impl Die {
-
     fn roll(&self, player: &Player) -> Vec<(Die, Player)> {
         match self {
             Determ { seed, side } => vec![(
@@ -55,20 +61,81 @@ impl Die {
                         .fold(0, |a, v| a + v),
                 ),
             )],
-            Hyper => vec![],
+            Hyper { comb: prev_comb } => vec![
+                // 3 (1,1,1): 1
+                (
+                    Hyper {
+                        comb: prev_comb * 1,
+                    },
+                    player.play(3),
+                ),
+                // 4 (2,1,1) (1,2,1) (1,1,2): 3
+                (
+                    Hyper {
+                        comb: prev_comb * 3,
+                    },
+                    player.play(4),
+                ),
+                // 5 (2,2,1) (2,1,2) (1,2,2) (3,1,1) (1,3,1) (1,1,3): 6
+                (
+                    Hyper {
+                        comb: prev_comb * 6,
+                    },
+                    player.play(5),
+                ),
+                // 6 (2,2,2) (1,2,3) (1,3,2) (2,1,3) (2,3,1) (3,1,2) (3,2,1): 7
+                (
+                    Hyper {
+                        comb: prev_comb * 7,
+                    },
+                    player.play(6),
+                ),
+                // 7 (1,3,3) (3,1,3) (3,3,1) (3,2,2) (2,3,2) (2,2,3): 6
+                (
+                    Hyper {
+                        comb: prev_comb * 6,
+                    },
+                    player.play(7),
+                ),
+                // 8 (2,3,3) (3,2,3) (3,3,2): 3
+                (
+                    Hyper {
+                        comb: prev_comb * 3,
+                    },
+                    player.play(8),
+                ),
+                // 9 (3,3,3): 1
+                (
+                    Hyper {
+                        comb: prev_comb * 1,
+                    },
+                    player.play(9),
+                ),
+            ],
+        }
+    }
+
+    fn get_comb(&self) -> u128 {
+        match self {
+            Hyper { comb } => *comb,
+            _ => 0,
         }
     }
 }
 
 fn read() -> (Player, Player) {
-    (Player::new(9, 0), Player::new(3, 0))
+    (Player::new(1, 9, 0), Player::new(2, 3, 0))
 }
 
 fn read_test() -> (Player, Player) {
-    (Player::new(4, 0), Player::new(8, 0))
+    (Player::new(1, 4, 0), Player::new(2, 8, 0))
 }
 
-fn play_until_score(win_score: usize, player1: &Player, player2: &Player) -> (Player, Player) {
+fn determ_play_until_score(
+    win_score: usize,
+    player1: &Player,
+    player2: &Player,
+) -> (Player, Player) {
     let mut die = Determ { seed: 0, side: 100 };
     let mut player1 = *player1;
     let mut player2 = *player2;
@@ -91,10 +158,34 @@ fn play_until_score(win_score: usize, player1: &Player, player2: &Player) -> (Pl
     (player1, player2)
 }
 
+fn hyper_play_until_score(
+    win_score: usize,
+    die: &Die,
+    player_up: &Player,
+    player_last: &Player,
+) -> (u128, u128) {
+    if player_last.get_score() >= win_score {
+        if player_last.id == 1 {
+            return (die.get_comb(), 0);
+        } else {
+            return (0, die.get_comb());
+        }
+    }
+
+    die.roll(player_up)
+        .iter()
+        .map(|(next_die, player_played)| {
+            hyper_play_until_score(win_score, next_die, player_last, player_played)
+        })
+        .fold((0 as u128, 0 as u128), |(p1a, p2a), (p1v, p2v)| {
+            (p1a + p1v, p2a + p2v)
+        })
+}
+
 #[test]
 fn day21pre_part1() {
     let (og_player1, og_player2) = read_test();
-    let (player1, player2) = play_until_score(1000, &og_player1, &og_player2);
+    let (player1, player2) = determ_play_until_score(1000, &og_player1, &og_player2);
 
     assert_eq!(
         739785,
@@ -105,7 +196,7 @@ fn day21pre_part1() {
 #[test]
 fn day21part1() {
     let (og_player1, og_player2) = read();
-    let (player1, player2) = play_until_score(1000, &og_player1, &og_player2);
+    let (player1, player2) = determ_play_until_score(1000, &og_player1, &og_player2);
 
     assert_eq!(
         1073709,
@@ -115,8 +206,25 @@ fn day21part1() {
 
 #[test]
 fn day21pre_part2() {
-    let p1_winner_universes = 444356092776315 as i64;
-    let p2_winner_universes = 341960390180808 as i64;
-    let total = p1_winner_universes + p2_winner_universes;
-    println!("{}", total);
+    let expect_p1_wins = 444356092776315 as u128;
+    let expect_p2_wins = 341960390180808 as u128;
+    let (player1, player2) = read_test();
+
+    let (p1_wins, p2_wins) = hyper_play_until_score(21, &Hyper { comb: 1 }, &player1, &player2);
+    println!("complete.");
+    assert_eq!((expect_p1_wins, expect_p2_wins), (p1_wins, p2_wins));
 }
+
+
+#[test]
+fn day21part2() {
+    let expect_p1_wins = 148747830493442 as u128;
+    let expect_p2_wins = 89305072914203 as u128;
+    let (player1, player2) = read();
+
+    let (p1_wins, p2_wins) = hyper_play_until_score(21, &Hyper { comb: 1 }, &player1, &player2);
+    println!("complete.");
+    assert_eq!((expect_p1_wins, expect_p2_wins), (p1_wins, p2_wins));
+}
+
+
